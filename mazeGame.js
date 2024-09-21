17c88d068faabcac57e69aa8938dbbbbf6397269
Reme7997 <00gonggong00@naver.com>
2024-09-21 21:10:42 +0900

    Update mazeGame.js

--- mazeGame.js
+++ mazeGame.js
@@ -1,1 +1,8 @@
-console.log("mazeGame.js 파일이 로드되었습니다.");
+// 로그를 화면에 표시하는 함수
+function displayLog(message) {
+  document.getElementById('log').innerText += message + '\n';
+}
+
+displayLog("mazeGame.js 파일이 로드되었습니다.");
+
+// 나머지 게임 코드...
